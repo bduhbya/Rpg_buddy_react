b37e15931fcf55fe2395485319d9c0b14e17125e
Brent Weatherall <Brentweatherall@gmail.com>
2024-01-11 22:11:09 -0800

    Add text color black to dialogue text
    Format with prettier

--- rpg-buddy/src/app/components/InitiativeInputDialog.tsx
+++ rpg-buddy/src/app/components/InitiativeInputDialog.tsx
@@ -18,12 +18,13 @@ const InitiativeInputDialog: React.FC<InitiativeInputDialogProps> = ({
     const [initiative, setInitiative] = useState<number>(0);
     const [name, setName] = useState<string>(character.name);
     const handleConfirm = () => {
-      const newCharacter: Character = {...character,
-         name: name,
-         initiative: initiative
+        const newCharacter: Character = {
+            ...character,
+            name: name,
+            initiative: initiative
         }
         onConfirm(
-          newCharacter
+            newCharacter
         );
     };
     const handleCancel = () => {
@@ -35,9 +36,9 @@ const InitiativeInputDialog: React.FC<InitiativeInputDialogProps> = ({
             <div className="bg-opacity-50 bg-gray-800 w-full h-full fixed"></div>
             <div className="modal bg-white p-8 rounded-lg z-10">
                 <h2 className="text-2xl font-bold mb-4 text-black">{"Enter Initiative"}</h2>
-                {duplicateEntryOrEmpty && <p className="mb-4">This character is already in the Combat list.</p>}
+                {duplicateEntryOrEmpty && <p className="mb-4 text-black">This character is already in the Combat list.</p>}
                 <label className="block mb-4">
-                  Character Name:
+                    Character Name:
                     <input
                         type="string"
                         className="w-full border rounded p-2 appearance-none focus:outline-none focus:border-blue-500 text-black"
@@ -59,7 +60,7 @@ const InitiativeInputDialog: React.FC<InitiativeInputDialogProps> = ({
                         className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-full"
                         onClick={handleConfirm}
                     >
-                      {"Add Character"}
+                        {"Add Character"}
                     </button>
                     <button
                         className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-full"
